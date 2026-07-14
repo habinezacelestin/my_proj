9f5642846a9333bafa1aa6d244e1fe2677eba8da
Node_project <habineza1111@gmail.com>
2026-07-14 12:52:19 +0200

    first push

---
+++ index.js
@@ -0,0 +1,25 @@
+var data=[{
+    name:"Celestin",
+    age:20,
+    city:"Paris",
+    country:"France"
+},{
+    name:"John",
+    age:30,
+    city:"New York",
+    country:"USA"
+},{
+    name:"Maria",
+    age:25,
+    city:"Madrid",
+    country:"Spain"
+}];
+
+// const getData = () => {
+//     return data;
+// }
+// console.log(getData());
+console.log(data);
+console.log(data[0].name);
+console.log(data[1].name);
+console.log(data[2].name);
